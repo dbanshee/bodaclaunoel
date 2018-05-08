a10ba2ffad57ff47d3d6d1abd2007e2f4202adb2
onamaya <onamaya@tedial.com>
2018-05-08 12:36:57 +0200

    Puerto HTTP

--- weeding.py
+++ weeding.py
@@ -104,4 +104,4 @@ def main():
 if __name__ == '__main__':
   app.debug = True
   app.config['SECRET_KEY'] = 'you-will-never-guess'
-  app.run(host='0.0.0.0',port=8000)
+  app.run(host='0.0.0.0',port=5000)
